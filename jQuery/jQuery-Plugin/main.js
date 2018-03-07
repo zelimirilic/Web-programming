4878d7527121e1b65a940e37039d73902fc00de0
zelimirilic <zelimir.ilic@gmail.com>
2018-03-07 21:59:55 +0100

    jQuery

--- jQuery/jQuery-Plugin/main.js
+++ jQuery/jQuery-Plugin/main.js
@@ -1,34 +1,9 @@
-/*$(function () {
-    
-    $("button").on("click",function(){
+$(function () {
+
+    $("button").on("click", function () {
         $("div").animate({
-            height:"+=500px",
-            width:"+=500px"
-        },1000);
+            height: "+=100px",
+            width: "+=100px"
+        }, 1000);
     })
 })
-*/
-
-$('.loop').owlCarousel({
-    center: true,
-    items: 2,
-    loop: true,
-    margin: 10,
-    responsive: {
-        600: {
-            items: 4
-        }
-    }
-});
-
-$('.nonloop').owlCarousel({
-    center: true,
-    items: 2,
-    loop: false,
-    margin: 10,
-    responsive: {
-        600: {
-            items: 4
-        }
-    }
-});
